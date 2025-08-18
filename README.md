38a5bc975504b0a6ab0e8b11082ff5a7eaeaa764
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-08-18 12:32:38 -0500

    Wrote and slightly styled the table. Then I wrote the array of books, the book constructor and the function for adding a book to the array. After that, I wrote the code for a button that displays all the books in the library. The instructions said to do it on page load but I made it a button instead as practice. I also started the coding for the button that adds a book to the library

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Library
-A project for improving my JS skills
+A project for improving my JS skills.
+
+This is the first project of the general JS course I'm doing. I wanted to speedrun 
+backend development in JS but it seems I need to get my fundamentals down if I 
+want to deal with API's and program server behaviour. So this will be one of 
+my first big steps toward becoming a full stack developer, or at least someone
+ who can contribute meaningfully to a team, whether that be on a general 
+software project, a hackathon, or anything else. Let's start!
---
+++ script.js
@@ -0,0 +1,73 @@
+const myLibrary = new Array();
+let allShown = false;
+
+function Book(title, author, noOfPages) {
+  this.title = title;
+  this.author = author;
+  this.noOfPages = noOfPages;
+  this.id = crypto.randomUUID();
+}
+
+function addBook(title, author, noOfPages){
+  book1 = new Book(title, author, noOfPages);
+  myLibrary.push(book1);
+  allShown = false;
+}
+
+function displayBooks (myLibrary){
+  if(allShown) return;
+
+  let bookTable = document.querySelector("#bookTable");
+  for(let book of myLibrary){ //retrieve each book from the book array
+    
+    //row
+    let row = document.createElement("tr");
+    //cells for each property
+    let title = document.createElement("td");
+    let author = document.createElement("td");
+    let noOfPages = document.createElement("td");
+    title.textContent = book.title; title.style.fontStyle = "italic";
+    author.textContent = book.author;
+    noOfPages.textContent = book.noOfPages;
+    //add to display, by adding cells to the row and the row to the table
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(noOfPages);
+    bookTable.appendChild(row);
+    
+  }
+  allShown = true;
+}
+
+addBook("Dune", "Bernie Mac", 800);
+addBook("The Great Mystery", "Mekala Salah", 408);
+addBook("Forward Future", "Jeffery McDennings", 723);
+
+showBookBtn = document.querySelector("#showBooks");
+
+showBookBtn.addEventListener("click", ()=>{
+  displayBooks(myLibrary);
+});
+
+//add button shows the form
+addBookBtn = document.querySelector("#addBook");
+addBookBtn.addEventListener("click", ()=>{
+  bookForm = document.querySelector("#bookForm");
+  bookForm.classList.remove("hidden");
+});
+//cancel button hides the form
+document.querySelector("#cancelBtn").addEventListener("click", ()=>{
+  bookForm = document.querySelector("#bookForm");
+  bookForm.classList.add("hidden");
+})
+
+//save book button adds book to the library
+saveBookBtn = document.querySelector("#saveBook");
+saveBookBtn.addEventListener("click", ()=>{
+  let titleEntry = document.querySelector("#titleEntry").value;
+  let authorEntry = document.querySelector("#authorEntry").value;
+  let pagesEntry = Number(document.querySelector("#pagesEntry").value);
+
+  addBook(titleEntry, authorEntry, pagesEntry);
+  alert(`The book "${titleEntry}" has been added successfully!`);
+})
